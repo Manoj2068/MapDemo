9239125fd251515a3a9b91267120b6798f3bbb07
Manoj2068 <mmahato.student@wust.edu>
2024-02-20 10:49:03 -0500

    First commit

---
+++ src/com/main/MapDemo.java
@@ -0,0 +1,22 @@
+package com.main;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Map<Integer, String> mp = new HashMap<>();
+		mp.put(101, "Manoj");
+		mp.put(102,"Hari");
+		mp.put(103, "Shyam");
+		
+		for (Map.Entry<Integer,String> ee:mp.entrySet())
+			System.out.println(ee.getKey()+"  "+ee.getValue());
+		
+
+	}
+
+}
